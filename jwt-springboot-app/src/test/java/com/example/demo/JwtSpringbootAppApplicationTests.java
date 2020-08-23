a4f5f15c8dccde62e34a16895197efc011a992ee
TechLeads <anupoju.anand@gmail.com>
2020-08-23 10:05:59 +0530

    Added test class

--- jwt-springboot-app/src/test/java/com/example/demo/JwtSpringbootAppApplicationTests.java
+++ jwt-springboot-app/src/test/java/com/example/demo/JwtSpringbootAppApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.techleads.app.JwtSpringbootAppApplication;
+
+@SpringBootTest(classes = JwtSpringbootAppApplication.class)
 class JwtSpringbootAppApplicationTests {
 
 	@Test
